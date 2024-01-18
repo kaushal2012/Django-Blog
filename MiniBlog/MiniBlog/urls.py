3f6e7c27f6a45bf125047c73d54c157ddeabb0f1
kaushal2012 <kaushalsinhvadher@gmail.com>
2024-01-18 15:09:10 +0530

    commit

--- MiniBlog/MiniBlog/urls.py
+++ MiniBlog/MiniBlog/urls.py
@@ -21,4 +21,9 @@ from blog import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
 ]
--- MiniBlog/blog/views.py
+++ MiniBlog/blog/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
+from faker import Faker
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    def generate_blog_text():
+        fake = Faker()
+        return fake.text(max_nb_chars=800)
+
+    return render(request, 'blog/home.html', {'blog_text': generate_blog_text()})
+
+
+def about(request):
+    return render(request, 'blog/about.html')
+
+
+def contact(request):
+    return render(request, 'blog/contact.html')
+
+
+def dashboard(request):
+    return render(request, 'blog/dashboard.html')
+
+
+def login(request):
+    return render(request, 'blog/login.html')
+
+
+def signup(request):
+    return render(request, 'blog/signup.html')
